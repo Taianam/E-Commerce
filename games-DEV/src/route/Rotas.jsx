7a4c0d83ae60b0b084004628880627be58272b8c
Taianam <taiana.monteirof@gmail.com>
2021-06-24 19:20:05 -0300

    deus na causa

--- games-DEV/src/route/Rotas.jsx
+++ games-DEV/src/route/Rotas.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Login from '../componets/login/Login';
 import Home from '../componets/home/Home';
 import Cadastro from '../componets/cadastro/Cadastro'
+import CrudProdutos from '../componets/controles/produtos/CrudProdutos';
 
 function Rotas() {
 	return (
@@ -15,6 +16,9 @@ function Rotas() {
 			</Route>
 			<Route path="/cadastro" exact>
 				<Cadastro />
+			</Route>
+			<Route path="/controle/produtos" exact>
+				<CrudProdutos />
 			</Route>
 		</Switch>
 	)
